3d1610c643d64ed72745be2b030d9a292a97be8d
juheehan@dbs.snu.ac.kr <juheehan@dbs.snu.ac.kr>
2024-10-01 23:50:15 +0900

    dfs

---
+++ baekjoon/dfs/11752.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+print(graph)
+
+visited = [0] * (n+1)
+
+def dfs(v):
+    for nx in graph[v]:
+        if visited[nx] == 0:
+            visited[nx] = v
+            dfs(nx)
+
+dfs(1)
+
+for i in range(2, n+1):
+    print(visited[i])
